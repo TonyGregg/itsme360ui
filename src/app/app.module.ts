d29c2f193d3137d98e381dd1e267959baeabfd35
TonyGregg <antonygenil@gmail.com>
2018-07-05 07:12:28 -0400

    Added Personal service and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PersonalService } from './services/personal.service';
+
 
 @NgModule({
   declarations: [
